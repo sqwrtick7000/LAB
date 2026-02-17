be37dc87b2fffffd7d7428bfad0beeeb94dfadab
sqwrtick7000 <kikomoro3.14@gmail.com>
2026-02-17 17:12:06 +0300

    Update KPO.cpp

--- KPO.cpp
+++ KPO.cpp
@@ -2,10 +2,19 @@
 #include <cmath>
 using namespace std;
 
-int main(){
-float m,n,a;
-cin>>n>>m>>a;
-float answer = ceil(m/a)*ceil(n/a);
-cout << answer<<endl;
-return 0;
+int main() {
+    double n, m, a;    
+    if (!(cin >> n >> m >> a)) {
+        cout << "Ошибка: Введите числовые значения!" << endl;
+        return 1;
+    }
+    if (n <= 0 || m <= 0 || a <= 0) {
+        cout << "Ошибка: Размеры должны быть больше нуля!" << endl;
+    } else {
+        long long stones_n = ceil(n / a);
+        long long stones_m = ceil(m / a);
+        long long answer = stones_n * stones_m;
+        cout << "Нужное количество плит: " << answer << endl;
+    }
+    return 0;
 }
