518d08a54834ee89360521dd09c80a7722420acd
sqwrtick7000 <kikomoro3.14@gmail.com>
2026-02-17 17:14:42 +0300

    Update KPO2.cpp

--- KPO2.cpp
+++ KPO2.cpp
@@ -1,11 +1,17 @@
-
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
     string x, res = "";
-    cin >> x;
+    if (!(cin >> x)) {
+        cout << "Ошибка чтения ввода!" << endl;
+        return 1;
+    }
+    if (x.empty()) {
+        cout << "Строка пуста!" << endl;
+        return 0;
+    }
     for (char c : x) {
         char lc = tolower(c);
         if (lc != 'a' && lc != 'o' && lc != 'y' && lc != 'e' && lc != 'u' && lc != 'i') {
@@ -13,6 +19,10 @@ int main() {
             res += lc;
         }
     }
-    cout << res << endl;
+    if (res == "") {
+        cout << "Результат пуст (все буквы были гласными)" << endl;
+    } else {
+        cout << res << endl;
+    }
     return 0;
 }
